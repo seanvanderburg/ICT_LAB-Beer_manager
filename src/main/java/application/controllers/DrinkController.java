3e18ceb3a8c9ba4834e0be1221ca141469a49a82
Sean van der Burg <seanvanderburg@msn.com>
2016-05-30 11:08:08 +0200

    Working web-database connection. Added insertion of drinks

--- src/main/java/application/controllers/DrinkController.java
+++ src/main/java/application/controllers/DrinkController.java
@@ -6,63 +6,72 @@ import application.models.DrinkDao;
 import org.springframework.ui.Model;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.servlet.ModelAndView;
 
 @Controller
-@RequestMapping(value="/drink")
+//@RequestMapping(value = "/drink")
 public class DrinkController {
 
-  @Autowired
-  private DrinkDao _drinkDao;
-  
-  @RequestMapping(value="/delete")
-  @ResponseBody
-  public String delete(long id) {
-    try {
-    	Drink drink = new Drink(id);
-      _drinkDao.delete(drink);
-    }
-    catch(Exception ex) {
-      return ex.getMessage();
-    }
-    return "User succesfully deleted!";
-  }
-  
-  @RequestMapping(value="/get-by-name")
-  @ResponseBody
-  public String getByName(String name) {
-    String drinkId;
-    try {
-      Drink drink = _drinkDao.getByName(name);
-      drinkId = String.valueOf(drink.getId());
-    }
-    catch(Exception ex) {
-      return "Drink not found";
-    }
-    return "The Drink id is: " + drinkId;
-  }
+	@Autowired
+	private DrinkDao _drinkDao;
 
-  @RequestMapping(value="/save")
-  @ResponseBody
-  public String create(String drinktype, String drinkname, String dateFounded, boolean available, Model model) {
-    try {
-      Drink drink = new Drink(drinktype, drinkname, dateFounded, available);
-      
-      _drinkDao.save(drink);
-    }
-    catch(Exception ex) {
-      return ex.getMessage();
-    }
-    return "Drink succesfully saved!";
-  }
-  
-  @RequestMapping(value="/", method=RequestMethod.GET)
-  public String showForm(Drink drink) {
-      return "form";
-  }
+	@RequestMapping(value = "/delete")
+	@ResponseBody
+	public String delete(long id) {
+		try {
+			Drink drink = new Drink(id);
+			_drinkDao.delete(drink);
+		} catch (Exception ex) {
+			return ex.getMessage();
+		}
+		return "User succesfully deleted!";
+	}
 
+	@RequestMapping(value = "/get-by-name")
+	@ResponseBody
+	public String getByName(String name) {
+		String drinkId;
+		try {
+			Drink drink = _drinkDao.getByName(name);
+			drinkId = String.valueOf(drink.getId());
+		} catch (Exception ex) {
+			return "Drink not found";
+		}
+		return "The Drink id is: " + drinkId;
+	}
 
-} // class UserController
+//	@RequestMapping(value = "/drink/save")
+//	@ResponseBody
+//	public String create(String drinktype, String drinkname,
+//			String dateFounded, boolean available) {
+//		try {
+//			Drink drink = new Drink(drinktype, drinkname, dateFounded,
+//					available);
+//
+//			_drinkDao.save(drink);
+//		} catch (Exception ex) {
+//			return ex.getMessage();
+//		}
+//		return "Drink succesfully saved!";
+//	}
+
+	@RequestMapping(value="/drink/save", method=RequestMethod.GET)
+    public String drinkForm(Model model) {
+        model.addAttribute("drink", new Drink());
+        return "drink";
+    }
+	
+	@RequestMapping(value = "/drink/save", method = RequestMethod.POST)
+	public String addDrink(@ModelAttribute("drink") Drink drink) {
+
+		_drinkDao.save(drink);
+
+		return "result";
+
+	}
+
+}
