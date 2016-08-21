6610bd0b88dea49a377e1b60d36319fcb53b071b
seanvdburg <seanvanderburg@msn.com>
2016-08-21 19:57:03 +0200

    fixed javadoc bug

--- src/main/java/application/models/BottleDao.java
+++ src/main/java/application/models/BottleDao.java
@@ -3,7 +3,6 @@ package application.models;
 import java.util.List;
 
 import javax.transaction.Transactional;
-
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.hibernate.criterion.Restrictions;
